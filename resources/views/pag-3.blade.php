2e4332c1b451214b191f9527a24083b25e60054f
salamone-gius <salamone.gius@gmail.com>
2022-07-06 17:29:17 +0200

    3 pages added and linked

--- resources/views/pag-3.blade.php
+++ resources/views/pag-3.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Laravel - Home</title>
+        <title>Laravel - Pagina 3</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -97,7 +97,7 @@
         </header>
         <main>
             <div class="main-content flex-center">
-                <h2 class="title">questa è la pagina <br> "home"</h2>
+                <h2 class="title">questa è la pagina <br> "3"</h2>
             </div>
         </main>
     </body>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/pag-1', function () {
+    return view('pag-1');
+});
+
+Route::get('/pag-2', function () {
+    return view('pag-2');
+});
+
+Route::get('/pag-3', function () {
+    return view('pag-3');
+});
